5e8e51107a6853f2f179ec12f28ca30be1f9f056
gamemakerr <96029891+gamemakerr@users.noreply.github.com>
2023-02-04 04:32:19 +0900

    끝

--- pages/person/[id].js
+++ pages/person/[id].js
@@ -7,19 +7,41 @@ export default function Sangha({ id }) {
       const results = await (
         await fetch(`https://billions-api.nomadcoders.workers.dev/person/${id}`)
       ).json();
-      console.log(results);
+
       setInformation([results]);
     })();
-  }, [id]);
+  }, []);
   console.log(information);
   return (
     <div className="container">
-      <img src={information[0].thumbnail} />
-      <h1>{information[0].id}</h1>
-      <div>{information[0].netWorth}</div>
-      <div>{information[0].industries[0]}</div>
-      <div>{information[0].about}</div>
-      <div>{information[0].bio}</div>
+      <img src={information[0]?.thumbnail} />
+      <h1>{information[0]?.id}</h1>
+      <div>{information[0]?.netWorth}</div>
+      <div>{information[0]?.industries[0]}</div>
+      <div>{information[0]?.about}</div>
+      <div>{information[0]?.bio}</div>
+      <div className="container2">
+        <h1>Financial Assets</h1>
+        {information[0]?.financialAssets.map((money, idx) => (
+          <div key={idx} className="box">
+            <div>{money.ticker}</div>
+            <div>{money.sharePrice}</div>
+          </div>
+        ))}
+      </div>
+      <style jsx>{`
+        .container {
+          background: white;
+          margin: 30px;
+        }
+        .container img {
+          margin: 50px;
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+      `}</style>
     </div>
   );
 }
